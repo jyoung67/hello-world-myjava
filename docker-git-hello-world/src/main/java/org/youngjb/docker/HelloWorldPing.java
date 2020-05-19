012f79b03fcd5e45f81fc6b7bb6625e364912e3a
jyoung67 <36745757+jyoung67@users.noreply.github.com>
2020-05-19 17:02:27 -0400

    Update HelloWorldPing.java

--- docker-git-hello-world/src/main/java/org/youngjb/docker/HelloWorldPing.java
+++ docker-git-hello-world/src/main/java/org/youngjb/docker/HelloWorldPing.java
@@ -3,8 +3,8 @@ package org.youngjb.docker;
 public class HelloWorldPing {
 	
 	public static void main(String args[]) throws Exception{
-		for(int i = 0 ; i < 100; i++){
-			System.out.println("Hello World Ping " + i );
+		for(int i = 0 ; i < 1000; i++){
+			System.out.println("Hello World Ping DOCKER" + i );
 			Thread.sleep(1000);
 		}
 	}
